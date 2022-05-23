972fa92074e3382f18c0ac9995f9e04fedbd181d
Henok R. Bedassa <jackbalageru@users.noreply.github.com>
2022-05-23 16:24:27 +0300

    Delete server directory

--- server/db.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-
-module.exports = () => {
-	const connection = mongoose
-		.connect(process.env.dbURI)
-		.then((result) => console.log("Connected to database"))
-		.catch((err) => console.log("could not connect to database"));
-};
