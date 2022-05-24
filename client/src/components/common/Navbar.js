317bf20cd49392a6bc9723beb2ab959441c73d50
jackbalageru <jackbalageru@yahoo.com>
2022-05-24 13:40:10 +0300

    MERN-CRUD-Frontend

---
+++ client/src/components/common/Navbar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+			<div className="container">
+				<NavLink className="navbar-brand" to="/">
+					Henok
+				</NavLink>
+				<button
+					className="navbar-toggler collapsed"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#mobileMenu"
+					aria-controls="mobileMenu"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="toggler-icon top-bar"></span>
+					<span className="toggler-icon middle-bar"></span>
+					<span className="toggler-icon bottom-bar"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="mobileMenu">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/cruds/new">
+								Create
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/cruds">
+								CRUDs
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/cruds/grid-view">
+								Grid View
+							</NavLink>
+						</li>
+						<li className="nav-item">
+							<NavLink className="nav-link" to="/cruds/list-view">
+								List View
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
